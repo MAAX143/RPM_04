55b601d8380b88e9fe5e2f0769fbaaeae9c3857d
DemonEyes12 <anemnonov2@gmail.com>
2022-02-02 09:05:23 +0300

    dictionary

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import os
 def input_path():
     pass
 
-def dictionary():
-    pass
+def dictionary(path, dict):
+    for filename in os.listdie(path): # Просматриваем файлы в папке
+        pathfile = path+'\\'+filename # Записываем путь к файлу в директории
+        dict[pathfile] = os.path.getsize(pathfile) # Записываем в словарь ключ - путь к файлу, значение - размер файла
+        if os.path.isdir(pathfile): # Проверяем, является ли файл папкой, если да, то...
+            dictionary(pathfile) # то снова запускаем функцию на проверку файлов в данной папке
 
 def duplucate():
     pass
