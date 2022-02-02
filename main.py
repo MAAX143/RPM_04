3b05930e8f7a658624952011dca66c871666bebe
DemonEyes12 <anemnonov2@gmail.com>
2022-02-02 09:24:59 +0300

    dictionary

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ def input_path():
 def dictionary():
     pass
 
-def duplucate():
-    pass
+def duplucate(dict):
+    dict_keys = []
+    dict_values = []
+    for t in dict.keys():
+        dict_keys.append()
+    for y in dict.values():
+        dict_values.append()
 
 def output():
     pass
