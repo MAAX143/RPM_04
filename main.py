39c3a853aa9fcae4e8efdb4c182013351bffe0f8
DemonEyes12 <anemnonov2@gmail.com>
2022-02-12 10:28:27 +0300

    о

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ def dictionary():
     pass
 
 def duplucate(dict):
-    dict_keys = []
-    dict_values = []
-    for t in dict.keys():
-        dict_keys.append()
-    for y in dict.values():
-        dict_values.append()
+    dict_keys = [] # Список для переменных
+    dict_values = [] # Список для значений переменных
+    for t in dict.keys(): # Прогоняем массив на переменные
+        dict_keys.append() # Записываем все переменные в список
+    for y in dict.values(): # Прогоняем массив на значения
+        dict_values.append() # Записываем все значения этих переменных в список
 
 def output():
     pass
