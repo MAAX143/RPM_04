372101cf4c68d541f50a84dc01628d03e2a30562
DemonEyes12 <anemnonov2@gmail.com>
2022-02-12 10:41:03 +0300

    о

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import os
 def input_path():
     pass
 
-def dictionary(path, dict):
-    for filename in os.listdie(path): # Просматриваем файлы в папке
-        pathfile = path+'\\'+filename # Записываем путь к файлу в директории
+def dictionary(path):
+    for filename in os.listdir(path): # Просматриваем файлы в папке
+        pathfile = os.path(path, dict) # Записываем путь к файлу в директории
         dict[pathfile] = os.path.getsize(pathfile) # Записываем в словарь ключ - путь к файлу, значение - размер файла
         if os.path.isdir(pathfile): # Проверяем, является ли файл папкой, если да, то...
             dictionary(pathfile) # то снова запускаем функцию на проверку файлов в данной папке
